b3c4dba515fa103a3586d05c4ae4b958044020fb
benfawcett01 <c0039304@newcastle.ac.uk>
2023-03-10 11:18:31 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/index.html")
+def back():
+    return render_template('index.html')
+
+@app.route("/results.html")
+def results():
+    return render_template('results.html')
